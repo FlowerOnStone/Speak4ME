21732880a7a42b37f2827eee5658d949ddf10124
hduoc2003 <21020059@vnu.edu.vn>
2023-04-10 00:40:07 +0700

    add storybook - a package for preview component

--- app/App.tsx
+++ app/App.tsx
@@ -2,10 +2,11 @@
 import React, {Component} from 'react';
 import { Alert, Text, View } from 'react-native';
 import { Icon } from './src/components/icons/icon-tag';
-import binIcon from './src/components/icons/test-gen-icon/bin-icon';
+import binIcon from './src/components/icons/save-icon';
 // import icon from './icon.test';
 
-export default function () {
-    return <Icon icon={binIcon}/>;
-}
+// export default function () {
+//     return <Icon icon={binIcon}/>;
+// }
 
+export {default} from './.storybook';
