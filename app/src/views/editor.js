ead62fcb9aabea9349745b1b9398555ad551f3fa
huetrantt21020018 <21020018@vnu.edu.vn>
2023-04-21 16:07:35 +0700

    chang style BaseFrame

--- app/src/views/editor.js
+++ app/src/views/editor.js
@@ -1,30 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   View,
+  Keyboard,
 } from 'react-native';
-
 
 import COLOR from '../constants/color';
 import BaseFrame from '../components/common/base-frame';
 import SuggestionBox from '../components/editor-screen/suggestionbox';
 
 import { useNavigation } from '@react-navigation/native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
 
 export default function Editor(props) {
 
   const navigation = useNavigation();
   const [sentences, setSentences] = useState([]);
 
+  const [baseFramePaddingTop, setBaseFramePaddingTop] = useState('30%');
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      (event) => {
+        setIsKeyboardVisible(true);
+        const { height } = event.endCoordinates;
+        setBaseFramePaddingTop(0);
+      },
+    );
+
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setIsKeyboardVisible(false);
+        setBaseFramePaddingTop('30%');
+      },
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
   const handleSave = (sentence) => {
-    // Nếu câu hiện tại không rỗng, thêm câu hiện tại vào mảng sentences
     if (sentence !== '') {
       setSentences([sentence, ...sentences]);
     }
@@ -40,7 +61,7 @@ export default function Editor(props) {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { paddingTop: baseFramePaddingTop }]}>
       <BaseFrame
         onSpeakButtonClick={handleSave}
         onViewHistoryClick={handleViewHistory}
@@ -48,19 +69,24 @@ export default function Editor(props) {
       />
       <SuggestionBox />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLOR.BACKGROUND,
-    alignItems: 'center',
+  },
+  contentContainer: {
+    flexGrow: 1,
+  },
+  innerContainer: {
+    flex: 1,
   },
   paragraph: {
-    flex: 10,
+    flex: 9,
   },
   suggestionbox: {
     flex: 1,
-  },
+  }
 });
