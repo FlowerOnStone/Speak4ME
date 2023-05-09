ae64c2792ee7dcc11540425291fca42e9f3675b7
hduoc2003 <hduoc2003@gmail.com>
2023-05-09 23:46:45 +0700

    change

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App.tsx';
+import App from './App';
 import {name as appName} from './app.json';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store.js';
